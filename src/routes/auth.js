2bd4822f2422f570f97fc77bb15ed16cbe27c2c7
Yorick Demichelis <yorick@techholding.co>
2022-04-18 16:06:45 -0700

    fix: add baseurl for google redirect uri

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,7 @@ import passport from 'passport'
 import controllers from '../controllers/index.js'
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, BASE_URL } = process.env
 
 passport.serializeUser((user, cb) => {
   process.nextTick(() => {
@@ -21,7 +21,7 @@ passport.deserializeUser((user, cb) => {
 const stategyOptions = {
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback',
+  callbackURL: `${BASE_URL}/auth/google/callback`,
   userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
   scope: ['profile', 'email'],
   state: false
